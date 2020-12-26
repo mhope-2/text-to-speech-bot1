4abaab736be437a59f3e5f1a1e68347cb849a896
mhope <mhope@npontu.com>
2020-12-26 08:33:46 +0000

    commit

--- ttsbot/ttsbot/settings.py
+++ ttsbot/ttsbot/settings.py
@@ -84,16 +84,16 @@ WSGI_APPLICATION = 'ttsbot.wsgi.application'
 
 # settings.py
    
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'postgres',
-#         'USER': 'postgres',
-#         'PASSWORD': 'postgres',
-#         'HOST': 'db',
-#         'PORT': 5432,
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
